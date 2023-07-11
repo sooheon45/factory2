80c34717acf3a251b971e53783b1cd152c0d2732
sooheon45 <sooheon45@uengine.org>
2023-07-11 06:40:42 +0000

    tt

--- Store/src/main/java/manufacture/domain/StoreRepository.java
+++ Store/src/main/java/manufacture/domain/StoreRepository.java
@@ -4,7 +4,6 @@ import manufacture.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel="stores", path="stores")
-public interface StoreRepository extends PagingAndSortingRepository<Store, >{
-
-}
+@RepositoryRestResource(collectionResourceRel = "stores", path = "stores")
+public interface StoreRepository
+    extends PagingAndSortingRepository<Store, Long> {}
